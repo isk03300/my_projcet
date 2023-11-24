57821e37ea4f69a90ae38e0896967cb22de1a123
isk03300 <isk03300@naver.com>
2023-11-24 11:54:48 +0900

    f

--- home_app.py
+++ home_app.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 def run_app_home():
 
-    st.header('2023년 서울특별시 부동산 실거래 동향 분석~')
+    st.header('2023년 서울특별시 부동산 실거래 동향 분석')
     
     st.image('https://t3.ftcdn.net/jpg/04/21/16/34/240_F_421163410_moniEVGDKoYdKXmJzIa34aWR3g0AeJUG.jpg',width=700)
