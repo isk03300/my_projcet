86697ded958ab5268964062c2072af411d47b858
isk03300 <isk03300@naver.com>
2023-11-27 09:45:43 +0900

    add

--- charts_app.py
+++ charts_app.py
@@ -6,23 +6,23 @@ import plotly.express as px
 import platform
 import seaborn as sb
 
-import platform
-from matplotlib import font_manager, rc
-plt.rcParams['axes.unicode_minus'] = False
-if platform.system() == 'Linux':
-    rc('font', family='NanumGothic')
-    
+# import platform
 # from matplotlib import font_manager, rc
 # plt.rcParams['axes.unicode_minus'] = False
+# if platform.system() == 'Linux':
+#     rc('font', family='NanumGothic')
 
-# if platform.system() == 'Darwin':
-#     rc('font', family='AppleGothic')
-# elif platform.system() == 'Windows':
-#     path = "c:/Windows/Fonts/malgun.ttf"
-#     font_name = font_manager.FontProperties(fname=path).get_name()
-#     rc('font', family=font_name)
-# else:
-#     print('Unknown system... sorry~~~~')
+from matplotlib import font_manager, rc
+plt.rcParams['axes.unicode_minus'] = False
+
+if platform.system() == 'Darwin':
+    rc('font', family='AppleGothic')
+elif platform.system() == 'Windows':
+    path = "c:/Windows/Fonts/malgun.ttf"
+    font_name = font_manager.FontProperties(fname=path).get_name()
+    rc('font', family=font_name)
+else:
+    print('Unknown system... sorry~~~~')
 
 
 
