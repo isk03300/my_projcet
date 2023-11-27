981db3041a6882a90c17a1a5543b4439c03263ee
isk03300 <isk03300@naver.com>
2023-11-27 10:18:39 +0900

    add

--- trade_app.py
+++ trade_app.py
@@ -13,19 +13,6 @@ from matplotlib import font_manager, rc
 plt.rcParams['axes.unicode_minus'] = False
 if platform.system() == 'Linux':
     rc('font', family='NanumGothic')
-    
-# from matplotlib import font_manager, rc
-# plt.rcParams['axes.unicode_minus'] = False
-
-# if platform.system() == 'Darwin':
-#     rc('font', family='AppleGothic')
-# elif platform.system() == 'Windows':
-#     path = "c:/Windows/Fonts/malgun.ttf"
-#     font_name = font_manager.FontProperties(fname=path).get_name()
-#     rc('font', family=font_name)
-# else:
-#     print('Unknown system... sorry~~~~')
-
 
 
 df = pd.read_csv('./data/dev.csv')
